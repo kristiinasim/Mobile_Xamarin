e2a3fa717b7f08c33a8f59cf7bcc5068b9be2849
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2019-09-19 09:49:40 +0300

    Base finished - change battery life

--- XamarinEssentials/MainActivity.cs
+++ XamarinEssentials/MainActivity.cs
@@ -3,19 +3,35 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Xamarin.Essentials;
+using System;
 
 namespace XamarinEssentials
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView _batteryLevelTextView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            var batteryLevel = Battery.ChargeLevel;
+
+            _batteryLevelTextView = FindViewById<TextView>(Resource.Id.textView2);
+            _batteryLevelTextView.Text = batteryLevel.ToString();
+            Battery.BatteryInfoChanged += Battery_BatteryInfoCharged;
+
         }
+
+        private void Battery_BatteryInfoCharged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            _batteryLevelTextView.Text = Battery.ChargeLevel.ToString();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
